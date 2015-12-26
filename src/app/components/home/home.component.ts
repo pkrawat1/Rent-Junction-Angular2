f3868ecf130aae257940252192c0fe5283571bce
Pankaj Rawat <pankajrawat19sept@gmail.com>
2015-12-26 23:47:45 +0530

    added home component as default route

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,28 +1,32 @@
 import { Component } from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import { RouterLink } from 'angular2/router';
-//import { Observable } from 'rxjs/Observable';
 import { DataService } from '../../services/data.service';
+import { ThumbnailPipe } from '../../pipes/thumbnail.pipe';
 
 @Component({
-  selector: 'categories',
+  selector: 'home',
   templateUrl: './app/components/home/home.html',
   directives: [CORE_DIRECTIVES, RouterLink],
-  providers: [DataService]
+  providers: [DataService],
+  pipes: [ThumbnailPipe]
 })
 
 export class HomeComponent{
-  title: String;
-  categories: Array<any>;
+  title: string;
+  categories: any[] = [];
 
   constructor(private _dataService: DataService) {}
 
-  ngOnInit() {
-    this.title = 'Categories';
-
+  getCategories(){
     this._dataService.getCategories()
         .subscribe((categories:any[]) => {
-          this.categories = this.categories = categories;
+          this.categories = categories;
         });
+  }
+
+  ngOnInit() {
+    this.title = 'Categories';
+    this.getCategories();
   }
 }
