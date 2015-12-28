cde6991057f43e8cce5b0d82b51645f595ec5229
Pankaj Rawat <pankajrawat19sept@gmail.com>
2015-12-28 17:46:00 +0530

    bug fixes

--- src/app/components/layout/header.component.ts
+++ src/app/components/layout/header.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
 
 @Component({
   selector: 'header',
-  templateUrl: './app/components/layout/header.html'
+  templateUrl: './app/components/layout/header.html',
+  directives: [RouterLink]
 })
 export class HeaderComponent {
 }
