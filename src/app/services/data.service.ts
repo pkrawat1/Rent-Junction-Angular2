904ab978d07103dc080ace92da93940504297009
Pankaj Rawat <pankajrawat19sept@gmail.com>
2016-06-06 21:16:27 +0530

    refactoring

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
+import {Observable} from 'rxjs/Observable';
+import {Category} from "../interfaces/category";
+import {Product} from "../interfaces/product";
 
 @Injectable()
 
 export class DataService {
+  _base_url : string;
 
   constructor(private http: Http) {
     this.http = http;
+    //this._base_url = 'http://localhost:3000/api/v1/';
+    this._base_url = 'https://rntjunc.herokuapp.com/api/v1/';
   }
 
-  getCategories() {
-    return this.http.get("http://localhost:3000/api/v1/categories.json")
-                    .map((response) => response.json());
+  getCategories() : Observable<Category[]>{
+    return this.http.get(this._base_url + "categories.json")
+                    .map((res : Response) => res.json());
   }
 
-  getProducts(subCategoryId: string) {
-    return this.http.get("http://localhost:3000/api/v1/categories/products/" + subCategoryId)
+  getProducts(subCategoryId: string) : Observable<Product[]> {
+    return this.http.get(this._base_url + "categories/products/" + subCategoryId)
                     .map((res: Response) => res.json());
   }
 
-  getProduct(productId: string) {
-    return this.http.get("http://localhost:3000/api/v1/products/" + productId)
+  getProduct(productId: string) : Observable<Product>{
+    return this.http.get(this._base_url + "products/" + productId)
                     .map((res: Response) => res.json());
   }
 
